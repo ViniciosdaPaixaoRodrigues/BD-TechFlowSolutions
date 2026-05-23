5e42a8a466de2c3d06b734b46db3e32c1ebd0c62
ViniciosdaPaixaoRodrigues <viniciospaixaoprofissional@gmail.com>
2026-05-23 16:09:59 -0300

    task: Configurado Html para funcionar com o Flask, e sistema de cadastro funcional em memória de maneira simples.

--- src/app.py
+++ src/app.py
@@ -8,21 +8,39 @@ usuarios = []
 def home():
     return render_template("index.html")
 
-@app.route("/cadastro", methods=["POST"])
+@app.route("/cadastro", methods=["GET", "POST"])
 def cadastrar():
-    dados = request.json
+    if request.method == "POST":
 
-    usuario = {
-        "nome": dados["nome"],
-        "email": dados["email"],
-        "senha": dados["senha"]
-    }
+        dados = request.form
 
-    usuarios.append(usuario)
+        usuario = {
+            "nome": dados["nome"],
+            "email": dados["email"],
+            "senha": dados["senha"]
+        }
 
-    return jsonify({
-        "mensagem": "Usuário cadastrado com sucesso!"
-    }), 201
+        usuarios.append(usuario)
+
+        return "Usuário cadastrado com sucesso!"
+
+    return render_template("cadastro.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        
+        email = request.form["email"]
+        senha = request.form["senha"]
+        
+        for usuario in usuarios:
+            
+            if usuario["email"] == email and usuario["senha"] == senha:
+                return f"Bem vindo, {usuario["nome"]}!"
+            
+        return "Email ou senha inválidos!"
+    
+    return render_template("login.html")
 
 @app.route("/usuarios", methods=["GET"])
 def listar_usuarios():
