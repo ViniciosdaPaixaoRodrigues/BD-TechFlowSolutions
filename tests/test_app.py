09f983764f5b04a33a1c02e6256a7854fdba83ba
ViniciosdaPaixaoRodrigues <viniciospaixaoprofissional@gmail.com>
2026-05-28 19:39:00 -0300

    Teste github actions

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.app import app
 from src.users import usuarios
